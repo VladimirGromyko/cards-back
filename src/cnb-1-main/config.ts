05f9384880a5a6115e1f14091960d2579a71e09a
VladimirGromyko <vl-mailbox@mail.ru>
2023-10-30 17:01:03 +0300

    first commit

---
+++ src/cnb-1-main/config.ts
@@ -0,0 +1,15 @@
+const USER_NAME = process.env.MONGO_DB_USER_NAME || "lesson5";
+const PASSWORD = process.env.MONGO_DB_USER_PASSWORD || "1qaz2wsx";
+const MONGO_DB_URL = process.env.MONGO_DB_URL || "cluster0.gbcdgvc.mongodb.net/nekobd"; // bd for tests
+
+export const MongoDBUris = `mongodb+srv://${USER_NAME}:${PASSWORD}@${MONGO_DB_URL}?retryWrites=true&w=majority`;
+console.log({MongoDBUris})
+
+export const DEV_VERSION = false;
+
+export const VERSION_2_0 = "/2.0";
+
+export const GMAIL_USER = "";
+export const GMAIL_PASS = "";
+
+export const PORT = 7542;
